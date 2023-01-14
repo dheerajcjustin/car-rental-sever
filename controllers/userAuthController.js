ec0b9c61207d51cc37e7b1f3d93ff779595841b7
dheerajcjustin <dheerajcjustin@gmail.com>
2023-01-14 22:11:46 +0530

    otp almost

--- controllers/userAuthController.js
+++ controllers/userAuthController.js
@@ -1,7 +1,10 @@
 const { User } = require("../models/userModel");
+const Token = require("../models/tokenModel");
 const JWT = require("jsonwebtoken");
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
+const crypto = require("crypto");
+const Joi = require("joi");
 const { response } = require("express");
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
@@ -20,7 +23,7 @@ const signupWithEmail = async (req, res) => {
   });
   if (userExits) {
     res
-      .status(202)
+      .status(409)
       .json({ message: "email,or mobile  already excites, signup failed" });
   } else {
     const hash = await bcrypt.hash(req.body.password, 5);
@@ -60,19 +63,23 @@ const otpVerify = async (req, res) => {
     const response = await otpVerifyFunction(otp, mobile);
     console.log("response of otp", response);
     if (response.status === true) {
-      const accessToken = await JWT.sign(
-        {
+      const user = await User.findOneAndUpdate({ mobile }, { verified: true });
+      const tokenData = {
+        role: "user",
+        userData: {
           name: user.name,
           email: user.email,
           userId: user._id,
           mobile: user.mobile,
         },
-        "paratuladapatti"
+      };
+      const accessToken = await JWT.sign(
+        tokenData,
+        process.env.ACCESS_TOKEN_SECRET
       );
-      await User.updateOne({ mobile }, { verified: true });
       res
         .status(201)
-        .json({ message: "otp verification successful", accessToken });
+        .json({ message: "otp verification successful", accessToken, user });
     } else {
       res.status(400).json({ message: " invalid otp verification " });
     }
@@ -84,59 +91,121 @@ const otpVerify = async (req, res) => {
 exports.otpVerify = otpVerify;
 
 const loginWithEmail = async (req, res) => {
+  console.log();
+  console.log();
+  console.log();
+
+  console.log("inside the login body is ", req.body);
   const { mobile, password } = req.body;
-  const user = await User.findOne({ mobile });
-  if (user) {
-    const validPass = await bcrypt.compare(password, user.password);
-    if (validPass) {
-      const accessToken = await JWT.sign(
-        {
+  try {
+    console.log("checking the user ");
+
+    const user = await User.findOne({ mobile });
+    if (user) {
+      console.log();
+      console.log("uer is  found ", user);
+      const validPass = await bcrypt.compare(password, user.password);
+      const tokenData = {
+        role: "user",
+        userData: {
           name: user.name,
           email: user.email,
           userId: user._id,
           mobile: user.mobile,
         },
-        "paratuladapatti"
-      );
-      return res.status(201).json({ message: "login successful", accessToken });
+      };
+      console.log("valid pass is comming ");
+
+      if (validPass) {
+        console.log("valid pass ");
+
+        const accessToken = await JWT.sign(
+          tokenData,
+          process.env.ACCESS_TOKEN_SECRET
+        );
+        console.log("login succes fuls ");
+        return res
+          .status(201)
+          .json({ message: "login successful", accessToken, user });
+      }
     }
+    console.log("invalid password or username");
+    res.status(400).json({ massage: "invalid user name of password" });
+  } catch (error) {
+    console.log("erreor");
+    console.log("somthing went worindg ");
+
+    console.log(error.massage);
+    res.status(400).json({ massage: "invalid user name of password" });
   }
-  res.status(400).json({ massage: "invalid user name of password" });
 };
 exports.loginWithEmail = loginWithEmail;
 
 const forgotPassword = async (req, res) => {
-  const user = await User.findOne({ mobile: req.body.mobile });
-  if (user) {
-    const response = await sendOtp(mobile);
-    const accessToken = await JWT.sign(
-      {
-        name: user.name,
-        email: user.email,
-        userId: user._id,
-        mobile: user.mobile,
-      },
-      "paratuladapatti"
-    );
-    res.status(201).json({ message: `otp send successfully at ${mobile}` });
-  } else {
-    res
-      .status(202)
-      .json({ message: `there is no user with mobile number${mobile}` });
+  try {
+    const user = await User.findOne({
+      mobile: req.body.mobile,
+      verified: true,
+    });
+    if (user) {
+      console.log("the user need to be forgot password", user);
+      const response = await sendOtp(mobile);
+      if (response.status === true) {
+        let passwordToken = await Token.findOne({ userId: user._id });
+        if (!passwordToken) {
+          passwordToken = await new Token({
+            userId: user._id,
+            token: crypto.randomBytes(32).toString("hex"),
+          }).save();
+        }
+      } else {
+        res.status(406).json({
+          message: `otp failed for  at ${mobile} contact developer`,
+        });
+      }
+      res.status(201).json({
+        message: `otp send successfully at ${mobile}`,
+        passwordToken: passwordToken.token,
+        userId: passwordToken.userId,
+      });
+    } else {
+      res
+        .status(400)
+        .json({ message: `there is no user with mobile number${mobile}` });
+    }
+  } catch (error) {
+    res.status(500).json("server addichu poy, call the developer");
   }
 };
+exports.forgotPassword = forgotPassword;
 
-const forgotPasswordPost = async (req, res) => {
-  const password = req.body.password;
-  if (password < 8) {
-    res.status(202).json({ message: "password must more than 8 character" });
-  } else {
-    try {
-      await User.updateOne();
-    } catch (error) {}
+const changePassword = async (req, res) => {
+  try {
+    const schema = Joi.object({ password: Joi.string().required() });
+    const { error } = schema.validate(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+    const { password, userId, token } = req.body;
+    const user = await User.findById(req.params.userId);
+    if (!user)
+      return res
+        .status(400)
+        .send("invalid at userId,  is no user with that userId or expired");
+
+    const passwordToken = await Token.findOne({
+      userId: userId,
+      token: token,
+    });
+    if (!token) return res.status(400).send("Invalid link or expired");
+    user.password = password;
+    await user.save();
+    await passwordToken.delete();
+    res.status(201).json("password changed successfully");
+  } catch (error) {
+    console.error(error);
+    res.status(500).json("server addichu poy, call the developer");
   }
 };
-
+exports.changePassword = changePassword;
 async function sendOtp(mobile) {
   mobile = Number(mobile);
 
@@ -148,14 +217,6 @@ async function sendOtp(mobile) {
   } catch (error) {
     return { status: false, error };
   }
-
-  // .then((verification) => {
-  //   console.log("verification chek send opt", verification_check.status);
-
-  //   return verification.status;
-  // });
-  console.log("verification", verification);
-  return { status: verification.status };
 }
 
 async function otpVerifyFunction(otp, mobile) {
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -4,6 +4,8 @@ const {
   signupWithEmail,
   loginWithEmail,
   otpVerify,
+  changePassword,
+  forgotPassword,
 } = require("../controllers/userAuthController");
 const validateReqBody = require("../middleware/validateReqBody");
 // const { verifyToken } = require("../helpers/authJwt");
@@ -19,5 +21,12 @@ router.post("/loginEmail", loginWithEmail);
 
 //body mobile and otp
 router.post("/otpVerify", otpVerify);
+
+// body mobile number user need to be changed
+//result will have passwordToken and userId both are need change the password
+router.post("/forgotPassword", forgotPassword);
+
+//body passwordToken  and userId and the newPassword
+router.post("/changePassword", changePassword);
 
 module.exports = router;
