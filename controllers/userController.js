7816868a16f7d2bca1153f4d3f21126618a0e0d4
dheerajcjustin <dheerajcjustin@gmail.com>
2023-02-20 13:53:17 +0530

    userProfile

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,6 @@
 const { Order } = require("../models/OrderModel");
 const { Vendor } = require("../models/vendorModel");
+const { User } = require("../models/userModel")
 const moment = require("moment");
 const mongoose = require("mongoose");
 const { tryCatch } = require("../utils/tryCatch");
@@ -53,4 +54,58 @@ exports.reportVendor = tryCatch(async (req, res) => {
     await Vendor.updateOne({ _id: vendor }, { $push: { reports: { message, carId } } })
     const elm = await Vendor.findOne({ _id: vendor });
     res.status(201).json(elm);
+})
+
+exports.getProfile = tryCatch(async (req, res) => {
+    const userId = req.user;
+    const profile = await User.findById(vendorId).select("name mobile profilePic")
+    res.send(201).json(profile);
+})
+
+exports.patchProfilePic = tryCatch(async (req, res) => {
+    const vendorId = req.user;
+    const { profilePic } = req.body;
+    if (!profilePic) return res.status(400).json("image is not found");
+    const profile = await User.findByIdAndUpdate(vendorId, { $set: { profilePic: profilePic } }).select("name mobile profilePic")
+    res.sendStatus(201);
+})
+
+
+
+exports.profilePatch = tryCatch(async (req, res) => {
+
+    console.log(req.body);
+    const { name, mobile, otp } = req.body;
+    const vendorId = req.user;
+    const response = await otpVerifyFunction(otp, mobile);
+    console.log("response of otp", response);
+    if (response.status === true) {
+        if (name && name.length > 2) {
+            await User.updateOne({ _id: vendorId }, { $set: { name: name, mobile: mobile } });
+        } else {
+            await User.updateOne({ _id: vendorId }, { $set: { mobile: mobile } });
+
+        }
+        res.status(201).json(req.body)
+    } else {
+        res.status(400);
+    }
+})
+exports.sendOtp = tryCatch(async (req, res) => {
+    const { name, mobile } = req.body;
+
+    console.log(mobile);
+    const checkMobile = await User.findOne({ mobile })
+    if (String(checkMobile._id) == req.user) {
+        const response = await sendOtp(mobile);
+        if (response.status === true) {
+            res.status(201).json({ mobile, name })
+        } else {
+            res.status(400).json({ message: response.status })
+        }
+    } else {
+        res.status(400).json("mobile number already exits");
+    }
+
+
 })
--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -1,5 +1,5 @@
 const { search, home, singleCar, } = require("../controllers/homeController");
-const { bookings, reportVendor } = require("../controllers/userController")
+const { bookings, reportVendor, getProfile, patchProfilePic, profilePatch, sendOtp } = require("../controllers/userController")
 const { verifyJWT } = require("../middleware/authJwt");
 const { userAuth } = require("../middleware/userAuth");
 const express = require("express");
@@ -10,6 +10,11 @@ router.get("/setup", home);
 router.get("/search", search);
 router.get("/bookings", verifyJWT, userAuth, bookings)
 router.post("/reportVendor", verifyJWT, userAuth, reportVendor)
+router.get("/profile", verifyJWT, userAuth, getProfile)
+router.patch("profilePic", verifyJWT, userAuth, patchProfilePic);
+
+router.post("/sendOtp", verifyJWT, userAuth, sendOtp);
+router.patch("/profile", verifyJWT, userAuth, profilePatch);
 
 
 module.exports = router;
